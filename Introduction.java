ff787825f670916bf300342c3267ad361a8e9b5a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-15 15:03:25 +0545

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,46 @@
+//1.java is the object oriented programming language developed by sun 
+     //microsystem in 1995
+
+//2. object-oriented means the capability to reuse code.
+
+//3.application is a program that run on the user's computers under the operating system.
+
+//4. applet is a small window based program that run on html pages
+
+//5. java virtual machine helps to converts the program into the special architectural neutral executable 
+      //program
+
+//6.  objects - is a instance of a class
+
+//7.  class - is a (blue print of a class)template for creating objects
+
+//8. procedural programming - divided into small parts called function
+      //follow top down approach
+
+//9. object oriented programming - program is divided into small parts called objects.
+      //follow bottom up approach
+
+//10.
+class Simple{
+    this is my first program
+    public static void main(String[]args){
+        System.out.println("hello world");
+    }
+}
+                //explanation of this code
+                class = class name;
+                Simple = class name;
+                //- comment
+                public = access modifier/specifier
+                static = keyword
+                void = method return type
+                main = method name
+                (String[]args) = main method
+
+//11.System.out.println("hello word")    
+    //System = class name
+    //out = object name
+    //println = method prints text on the screen
+    //hello world = text to be printed on the screen
+
+//12. method overriding = mismatch of the method name.    

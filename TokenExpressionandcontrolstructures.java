ae2eecc7e1cfe0e6d56ca7043a623674dd6376b5
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-16 22:41:21 +0545

    Add files via upload

---
+++ TokenExpressionandcontrolstructures.java
@@ -0,0 +1,50 @@
+//1. Data type is keyword used to allocate sufficient memory space for the data
+    //data type is used for representing the data in main memory(RAM)
+
+// 2.  fundamental or primitive data type
+// 3.  Derived data type
+//4. user defined data type
+
+//2. primitive data type 
+  //* boolean data type */
+  // boolean data type is used to store true or false value
+  // boolean x = false
+
+  //*Byte Data type */
+  // byte data type is used to store the integer value between -128 to 127
+  // byte x = 127
+
+  //*short Data type */
+  // short data type is used to store the integer value between -32768 to 32767
+  // short x = 32767
+
+  //*Int Data type */
+  // int data type is used to store the integer value between -2147483648 to 2147483647
+  // int x = 2147483647
+
+  //*Long Data type */
+  // long data type is used to store the integer value between -9223372036854775808 to 9223372036854775807
+  // long x = 9223372036854775807
+
+  //*Float Data Type */
+  // float data type is used to store the floating point value between -3.4E+38 to 3.4E+38
+  // float x = 3.4E+38
+
+  //*Double Data type */
+  // double data type is used to store the floating point value between -1.7E+308 to 1.7E+308
+  // double x = 1.7E+308
+
+  //*Char Data type */
+  // char data type is used to store the character value between 0 to 65535
+  // char x = 65535
+
+  //*Derived Data Type */
+  // Derived data type is a data type that is derived from the primitive data type
+
+  //*user defined data types */
+  // user defined data type is a data type that is created by the user
+
+  // 5. variable - is an identifier which can be used to identify input data in a program.
+    //rueles- variable name start with alphabets,no space is allowed
+
+  // 6.   

052e1dd35157d72e24d215b68bde2d4837967527
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-25 13:16:51 +0545

    Add files via upload

--- OopConcept.java
+++ OopConcept.java
@@ -1,3 +1,22 @@
-public class OopConcept {
-    
-}
+//OOP - object oriented 
+// object = data + method
+//object- any entity that has state and behavior is known as an object.
+//Component of class 
+//method overloading - if we defined many method with same name but with diffrent in parameters then this process is called method overloading.
+//constructors - three rules.
+  //1. constructor name must be same as its class name.
+  //2. constructor do not have a return type
+  //3. constructor are invoked using the new operator when an object is created.
+
+//default constructor- a constructor that has no parameter is known as default constructor.
+//parameterized construtor - a constructor that has  parameter is known as parameterized constructor.
+//constructor overloading-
+//copy constructor-
+//'this' - this keyword refers to the current object in a method or constructor.
+//static keywoard- static keyword is used for memory management.
+   //variable-if we declare any variable as static known as static variable.
+   //method - if we apply static keyword with any method known as static method.
+   //block - used for static initialization of class.
+   //nested class  -  
+
+// Recursion - is a  process by which a a function calls itself repeatedly until some specified condition has been satisfied.  

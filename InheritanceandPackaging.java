7114241c62fdf44734376c318190b18ca42fb542
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-06-03 20:59:46 +0545

    Add files via upload

---
+++ InheritanceandPackaging.java
@@ -0,0 +1,35 @@
+//1.inheritance -  the procedure or mechanism of acquiring all the properties and behavior of one class to another.
+
+//2.extend keyword - keywoard used to inherit the properties of a class.
+//types of inheritances
+    //1.single inheritances - a single class gets derived from its base class.ex
+    //2.multi-level inheritance-  a derived class gets created from another derived class and can have any number of levels.
+    //3.Hierarchical inheritance- two or more classes inherit the properties of one existing class.
+
+//3.why multiple inheritance is not supported in java? ans: To reduce the complexity and simplify the language, multiple inheritance is not supported in java.
+
+//4. 'SUPER' keywoard in java is a reference variable that is used to refer parent class object.
+
+//5.METHOD OVERRIDING - The process of redefining the inherited method of the super class in the derived class is called method overriding.
+
+//6. COMPILE TIME POLYMORPHISM -
+
+//7. RUN TIME POLYMORPHISM- 
+
+//8. 'ABSTRACT' - is a keyword used to create a class as an abstract class and it can contain abstract method.
+                    //eg.
+                    public abstract class A{
+                        ............
+                        public abstract void myMethod();
+                        void myMethod(){
+
+                        }
+                    }
+                    
+//9. INTERFACE - blueprint of a class or referecnce type 
+
+//10. Final  class-
+
+//11. Abstract class-
+
+//12. PACKAGE - is a mechanism to encapsulate a group of classes, interfaces and sub packages which is used to providing access protection and namespace management and to make searching/locating and using of classes, interfacess, enumerations and annotations easier.
